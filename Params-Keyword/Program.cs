1f3323f4fb633ec51ef233a68a24a2b7cc2e0fd4
janthony25 <junanthony.business@gmail.com>
2024-02-01 18:29:10 +1300

    Add project files.

---
+++ Params-Keyword/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ParamsKeyword
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // params keyword = a method parameter that takes a variable number of arguments.
+            //                  The parameter type must be a single - dimensional array
+
+            double total = CheckOut(1, 4, 5, 10, 3.24);
+
+            Console.WriteLine(total);
+
+            Console.ReadKey();
+        }
+
+       static double CheckOut (params double[] prices)
+        {
+            double total = 0;
+
+            foreach (double price in prices)
+            {
+                total += price;
+            }
+
+            return total;
+        }
+    }
+}
